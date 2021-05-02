4cd0104ded8ab41063b5ec9997cc411ed578f9b7
Joshua Lopez <jrenelopez92@gmail.com>
2021-05-01 21:58:35 -0500

    floats and double

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// floating and double
+
+
+    float myMinLFloatValue = Float.MIN_VALUE;
+    float myMaxFloatValue = Float.MAX_VALUE;
+
+    System.out.println("Float minimum value  " + myMinLFloatValue); // 1.4E-45
+    System.out.println("Float Max value  " + myMaxFloatValue); // 3.4028235E38
+
+
+    double myMinLDoubleValue = Double.MIN_VALUE;
+    double myMaxDoubleValue = Double.MAX_VALUE;
+
+    System.out.println("Double minimum value  " + myMinLDoubleValue); // 4.9E-324
+    System.out.println("Double Max value  " + myMaxDoubleValue); //  1.7976931348623157E308
+
+    int myIntValue = 5 / 3;
+    float myFloatValue = 5f / 3f; // or we can do 5.25f
+    double myDoubleValue = 5d / 3d; //double myDoubleValue = 5.00 / 3.00; //use double try not to use float anymore in Java
+    System.out.println("MyIntValue " + myIntValue );
+    System.out.println("MyFloatValue " + myFloatValue);
+    System.out.println("MyDoubleValue " + myDoubleValue );
+    // doubles are processed faster one modern computers this is a big reason why we need to use double and not float
+    // double is also the default
+
+        
+
+
+
+    }
+}
--- PrimitiveTypesChallenge/src/com/company/Main.java
+++ PrimitiveTypesChallenge/src/com/company/Main.java
@@ -3,25 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// Your challenge is to create a byte variable and set it to any valid  byte number, it doesn't matter.
+        // Your challenge is to create a byte variable and set it to any valid byte
+        // number, it doesn't matter.
         // crate a short variable adn set it to any valid short number.
         // create an int variable and set it to any valid short number.
 
-        // create and int varaibale and set it to any valid int number . Lastly , create a variable of type long
-        // and make it equal to 50000 plus 10 times the sum of the byte plus the short plus the integer values.
+        // create and int variable and set it to any valid int number . Lastly , create
+        // a variable of type long
+        // and make it equal to 50000 plus 10 times the sum of the byte plus the short
+        // plus the integer values.
 
         byte myByte = 50;
         short myShort = 1000;
         int myInt = 30000;
 
-        long myLongTotal =  (50000L + 10L * (myByte + myShort + myInt )); // we don't have to cast here because longs
+        long myLongTotal = (50000L + 10L * (myByte + myShort + myInt)); // we don't have to cast here because longs
         // know that any of the other number data types will fit here.
-
 
         System.out.println(" Test test " + myLongTotal);
 
-        short shortTotal = (short) (1000 + 10 + (myByte + myShort + myInt ));
-        // for this one we have to cast because the short doesn't know if I can accept a number form int since it
+        short shortTotal = (short) (1000 + 10 + (myByte + myShort + myInt));
+        // for this one we have to cast because the short doesn't know if I can accept a
+        // number form int since it
         // maybe to large
     }
 }
